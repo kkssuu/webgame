1116060af1e25e65879e0eee429f19fddfb0f272
kkssuu <155282573+kkssuu@users.noreply.github.com>
2023-12-31 13:46:05 +0900

    Add files via upload

---
+++ src_game.py
@@ -0,0 +1,20 @@
+import random
+
+lb_comp=Element("lb_comp")
+
+def scissors(*args):
+    rand()
+def rock(*args):
+    rand()
+def paper(*args):
+    rand()
+
+def rand(*args):
+
+    a=random.randrange(0,3)
+    b=["가위","바위","보"]
+
+    lb_comp.element.innerText=b[a]
+    
+
+
